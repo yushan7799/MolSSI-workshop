03cc615e3728cacbe3d3c26454143c75eb27a889
Yushan Zhang <yushan7799@gmail.com>
2018-07-19 17:41:14 -0400

    Implement testing

--- fommscool/tests/test_fommscool.py
+++ fommscool/tests/test_fommscool.py
@@ -1,12 +1,35 @@
-"""
-Unit and regression test for the fommscool package.
-"""
 
-# Import package, test suite, and other packages as needed
+"""
+Testing the integrate package
+"""
 import fommscool
 import pytest
+import numpy as np
 import sys
 
 def test_fommscool_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "fommscool" in sys.modules
+
+def g(x):
+    return 3.0 * x
+
+def f(x):
+    return np.power(x, 2)
+
+def h(x):
+    return np.ones(x.size)
+
+def volume(x):
+    squares = np.power(x, 2)
+    return np.sum(squares, axis=1)
+
+def test_trapz():
+     x = np.array([0, 10])
+     I = fommscool.newton_cotes.trapz(x, g)
+     assert I == 150.00
+ 
+#def test_monte1d():
+#     x = np.array([0, 3])
+#    I = fommscool.stochastic.monte_1d(x, f, 100000)
+#    assert np.allclose(I, 9.00, 1e-2)

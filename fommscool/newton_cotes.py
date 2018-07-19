ccd01723f56b3e206d99fc4c0c2dca747472c03b
Yushan Zhang <yushan7799@gmail.com>
2018-07-19 16:21:03 -0400

    add newton_cotes

--- fommscool/newton_cotes.py
+++ fommscool/newton_cotes.py
@@ -1,34 +1,27 @@
 """
-newton_cotes.py
-Integrate package for MolSSI wrkshop at FOMMS 2018
-
-Handles the primary functions
+This file contains the implementation of the trapezoidal rule
 """
 
+import numpy as np
 
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format)
-
-    Replace this function and doc string for your own project
-
+def trapz(x, f):
+    """ 
+    Compute a 1D definite integral using the trapezoidal rule
     Parameters
     ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
-
+    f : function
+        User defined function.
+    x : numpy array
+        Integration domain.
     Returns
     -------
-    quote : str
-        Compiled string including quote and optional attribution
-    """
+    I : float
+        Integration result.
+    """   
+    a = x[0]
+    b = x[1]
+    ya = f(a)
+    yb = f(b)
+    I = (b-a) * (ya + yb) / 2
+    return I
 
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
-
-
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())

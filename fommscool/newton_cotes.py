5e2756db749f835461a6534d0a11460e3b9042fa
yushan zhang <yushan7799@gmail.com>
2018-07-27 13:10:45 -0400

    updated stochastic code to 2D

--- fommscool/newton_cotes.py
+++ fommscool/newton_cotes.py
@@ -25,3 +25,24 @@ def trapz(x, f):
     I = (b-a) * (ya + yb) / 2
     return I
 
+def simpson(x, f):
+    """ 
+    Compute a 1D definite integral using the simpson's rule.
+    Parameters
+    ----------
+    f : function
+        User defined function.
+    x : numpy array
+        Integration domain.
+    Returns
+    -------
+    I : float
+        Integration result.
+    """   
+    a = x[0]
+    b = x[1]
+    ya = f(a)
+    yb = f((a+b)/2)
+    yc = f(b)
+    I = (b-a) * (ya + 4 * yb + yc) / 6
+    return I
--- fommscool/stochastic.py
+++ fommscool/stochastic.py
@@ -25,3 +25,29 @@ def monte_1d(x, f, trials):
    d = (b - a) *  np.random.rand(1, trials) + a
    y = f(d)
    return (b-a) * np.sum(y) / trials
+
+
+def monte_2d(f, v, domain, trials):
+    """
+    Compute area of circle using Monte Carlo sampling
+    Parameters
+    ----------
+    f : function
+        User defined function.
+    domain : numpy array
+        Integration domain.
+    v : function
+        Integration volume .
+    Returns
+    -------
+    I : float
+        Integration result.
+    """
+
+    area = np.prod(domain[1] - domain[0])
+    matrix = np.sqrt(area) * np.random.rand(trials, 2) + np.min(domain)
+    v_eval = v(matrix)
+    idx = np.where(v_eval < 1.0)
+    points_inside = v_eval[idx]
+    k = points_inside.size
+    return area * k / trials
